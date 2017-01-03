ef1709452e423922503a26fd872ab21642fa88ec
peb9 <Preston Breedon-Glen@DESKTOP-F3POMGC>
2017-01-03 03:48:22 -0500

    Hello I Am Preston

---
+++ src/java/MyChild.java
@@ -0,0 +1,8 @@
+package java;
+public class MyChild extends MyParent {
+	
+	@Override
+	public void method1(Integer _inp) throws MyException1Child {
+	
+	}
+}
---
+++ src/java/MyParent.java
@@ -0,0 +1,8 @@
+package java;
+public class MyParent {
+	public void method1(Integer _inp) throws MyException1 {
+	}
+	public void method2(Integer _inp) throws MyException1Child {
+	}
+
+}
